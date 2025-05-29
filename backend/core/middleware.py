d02fc7bdd9e6ce3199b26f6e743131f20f1e2d7a
KreativeThinker <sagely2005eagle@gmail.com>
2025-05-29 17:44:55 +0530

    feat: add middleware for frontend document rendering

---
+++ backend/core/middleware.py
@@ -0,0 +1,8 @@
+class FrameOptionsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response["X-Frame-Options"] = "ALLOW-FROM http://localhost:3000"
+        return response
