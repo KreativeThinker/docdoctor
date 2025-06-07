b14928cc48a7c9264f46938c6f4fc836c93e1905
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-07 13:35:17 +0530

    feat(backend): migrate backend
    Move DRF backend to backend_old
    
    BREAKING CHANGE: Migrate backend

--- backend_old/core/middleware.py
+++ backend_old/core/middleware.py
@@ -1,8 +1,11 @@
+from core import settings
+
+
 class FrameOptionsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        response["X-Frame-Options"] = "ALLOW-FROM http://localhost:3000"
+        response["X-Frame-Options"] = f"ALLOW-FROM {settings.FRONTEND_URL}"
         return response
