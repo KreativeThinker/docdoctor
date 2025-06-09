d5fe2da3abac2db5cae43e292ed850900efae4d4
KreativeThinker <sagely2005eagle@gmail.com>
2025-06-09 13:36:14 +0530

    fix(backend): add static file hosting on /media
    closes #8

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api import document
 from app.core import config
@@ -10,10 +11,18 @@ from prisma import Prisma
 
 app = FastAPI()
 db = Prisma(auto_register=True)
+print(config.MEDIA_DIR)
+print(list(config.MEDIA_DIR.iterdir()))
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+
+    app.mount(
+        "/media",
+        StaticFiles(directory=str(config.MEDIA_DIR.resolve())),
+        name="media",
+    )
     await db.connect()
     await init_client()
     yield
